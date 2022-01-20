2148129047fec98656993933ad8d113a6e6cb9ee
robbert-sudo <robbert_de_Die@hotmail.com>
2022-01-20 21:30:37 +0100

    uploadpage fixes

--- src/pages/UploadGame.js
+++ src/pages/UploadGame.js
@@ -2,13 +2,14 @@ import React, {useContext, useEffect, useState} from "react";
 import axios from "axios";
 import {useHistory} from "react-router-dom";
 import {AuthContext} from "../context/AuthContext";
+import "./UploadGame.css";
 
 function UploadGame() {
     //state voor het formulier
     const [gameName, setGameName] = useState(null);
     const [system, toggleSystem] = useState("");
     const [developer, setDeveloper] = useState("");
-    const [price, setPrice] = useState();
+    const [price, setPrice] = useState(null);
     const [postImage, setPostImage] = useState({
        image: "",
     });
@@ -34,25 +35,29 @@ function UploadGame() {
         console.log(postImage.image);
 
         if (gameName !== null) {
-            console.log(user.user_id);
-        try {
-            await axios.post('http://localhost:8080/games', {
-                "name": gameName,
-                "system": system,
-                "developer": developer,
-                "uploader_id": user.user_id,
-                "uploader_name": user.username,
-                "price": price,
-                "image": postImage.image,
-            },{
-                cancelToken: source.token,
-            });
-            history.push("games");
-        } catch (e) {
-            console.error(e);
-        }
-    } else {
-        console.log("naam is een verplicht veld!");
+            if (price !== null) {
+                console.log(user.user_id);
+                try {
+                    await axios.post('http://localhost:8080/games', {
+                        "name": gameName,
+                        "system": system,
+                        "developer": developer,
+                        "uploader_id": user.user_id,
+                        "uploader_name": user.username,
+                        "price": price,
+                        "image": postImage.image,
+                    }, {
+                        cancelToken: source.token,
+                    });
+                    history.push("games");
+                } catch (e) {
+                    console.error(e);
+                }
+            } else {
+                console.log("naam is een verplicht veld!");
+            }
+        } else {
+            console.log("prijs is een verplicht veld.");
         }
     }
 
@@ -88,44 +93,64 @@ function UploadGame() {
             <form className="gameuploaden"
                   onSubmit={handleGameUpload}>
                 <p>Invoervelden</p>
+
+                <div className="namefield">
+                <label for="gamename">Game name(verplicht)</label>
                 <input
+                    id="gamename"
                     type="text"
                     name="gamename"
                     onChange={(e) => setGameName(e.target.value)}
-                    placeholder="GameName"
+                    placeholder="GameName(verplicht)"
                 />
+                </div>
+
+                <div className="systemfield">
+                    <label for="systemname">systeem</label>
                 <input
+                    id="systemname"
                     type="text"
                     name="system"
                     onChange={(e) => toggleSystem(e.target.value)}
                     placeholder="system"
                 />
+                </div>
+
+                <div className="developerfield">
+                    <label for="developername">developer</label>
                 <input
+                    id="developername"
                     type="text"
                     name="developer"
                     onChange={(e) => setDeveloper(e.target.value)}
                     placeholder="developer"
                 />
+                </div>
+
+                <div className="pricefield">
+                    <label for="price">prijs</label>
                 <input
+                    id="price"
                     type="number"
                     name="price"
                     step="0.01"
                     min="0"
                     onChange={(e)=> setPrice(e.target.value)}
-                    placeholder="prijs"
+                    placeholder="prijs(verplicht)"
                 />
-
-
-                {/*//uploader_id meegeven, geen keuze of optie hierin*/}
+                </div>
 
 
 
+                <label for="Image">foto upload</label>
                 <input
+                    id="Image"
                     type="file"
                     name="myImage"
                     accept=".jpeg, .png, .jpg"
                     onChange={(e) => handleFileUpload(e)}
                 />
+
 
                 <button
                     type="submit"
