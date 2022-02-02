aa1635e49cd697ddbae3759ee87dd4b862debe0e
robbert-sudo <robbert_de_Die@hotmail.com>
2022-02-02 18:29:38 +0100

    UploadGame.js_htmlFor_ipv_for

--- src/pages/RatingPage.js
+++ src/pages/RatingPage.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from "react";
-import {useHistory, useParams} from "react-router-dom";
+import {useParams} from "react-router-dom";
 import axios from "axios";
 
 function RatingPage() {
